a1de11e47a359fbf034f8bf60e984ba46a40bbf7
Romain Verduci <romain.verduci@gmail.com>
2012-11-05 23:18:54 +0100

    Dowload collectd sources from internet then install

--- install-collectd.sh
+++ install-collectd.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#Create a temp dir for provisioning sources
-#tempDir=`mktemp -dt provXXX`
+Create a temp dir for provisioning sources
+tempDir=`mktemp -dt provXXX`
 
-#echo "Downloading collectd rpm"
-#wget --progress=bar --output-document="$tempDir/collectd-5.1.0-1.el6.rft.x86_64.rpm" "http://pkgs.repoforge.org/collectd/collectd-5.1.0-1.el6.rft.x86_64.rpm"
-#wget --progress=bar --output-document="$tempDir/perl-Collectd-5.1.0-1.el6.rft.x86_64.rpm" "http://pkgs.repoforge.org/collectd/perl-Collectd-5.1.0-1.el6.rft.x86_64.rpm"
-#wget --progress=bar --output-document="$tempDir/perl-Regexp-Common-2010010201-2.el6.rf.noarch.rpm" "http://pkgs.repoforge.org/perl-Regexp-Common/perl-Regexp-Common-2010010201-2.el6.rf.noarch.rpm"
-#wget --progress=bar --output-document="$tempDir/libstatgrab-0.15-1.el6.rf.x86_64.rpm" "http://pkgs.repoforge.org/libstatgrab/libstatgrab-0.15-1.el6.rf.x86_64.rpm"
+echo "Downloading collectd rpm"
+wget --progress=bar --output-document="$tempDir/collectd-5.1.0-1.el6.rft.x86_64.rpm" "http://pkgs.repoforge.org/collectd/collectd-5.1.0-1.el6.rft.x86_64.rpm"
+wget --progress=bar --output-document="$tempDir/perl-Collectd-5.1.0-1.el6.rft.x86_64.rpm" "http://pkgs.repoforge.org/collectd/perl-Collectd-5.1.0-1.el6.rft.x86_64.rpm"
+wget --progress=bar --output-document="$tempDir/perl-Regexp-Common-2010010201-2.el6.rf.noarch.rpm" "http://pkgs.repoforge.org/perl-Regexp-Common/perl-Regexp-Common-2010010201-2.el6.rf.noarch.rpm"
+wget --progress=bar --output-document="$tempDir/libstatgrab-0.15-1.el6.rf.x86_64.rpm" "http://pkgs.repoforge.org/libstatgrab/libstatgrab-0.15-1.el6.rf.x86_64.rpm"
 
 
 echo "Installing collectd rpm"
-yum install -y collectd/collectd-5.1.0-1.el6.rft.x86_64.rpm collectd/libstatgrab-0.15-1.el6.rf.x86_64.rpm collectd/perl-Collectd-5.1.0-1.el6.rft.x86_64.rpm collectd/perl-Regexp-Common-2010010201-2.el6.rf.noarch.rpm
+yum install -y $tempDir/collectd-5.1.0-1.el6.rft.x86_64.rpm $tempDir/libstatgrab-0.15-1.el6.rf.x86_64.rpm $tempDir/perl-Collectd-5.1.0-1.el6.rft.x86_64.rpm $tempDir/perl-Regexp-Common-2010010201-2.el6.rf.noarch.rpm
